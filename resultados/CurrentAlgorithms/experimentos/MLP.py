11fc04ded960d0d5468ac1c040793d206aa0ad6b
Gabriela Ramalho <gabrielaramalho96@gmail.com>
2019-11-06 19:30:07 -0300

    Updating MLP algorithm and related functions to validate results

--- resultados/CurrentAlgorithms/experimentos/MLP.py
+++ resultados/CurrentAlgorithms/experimentos/MLP.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from plot_learning_curve import plot_learning_curve
+# from plot_learning_curve import plot_learning_curve
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import learning_curve
 from run import write_csv, save_hist
@@ -27,12 +27,11 @@ def get_params(x_train, y_train):
     clf2.fit(x_train, y_train)
     return clf2.best_params_
 
-def plot_validation_curve(X,y):
+def plot_validation_curve(X,y, param, param_range):
     # Reference: https://scikit-learn.org/stable/auto_examples/model_selection/plot_learning_curve.html
     from sklearn.model_selection import validation_curve
-    param_range = np.logspace(-6, -2, 4)
-    
-    train_scores, test_scores = validation_curve(MLPClassifier(), X, y, "learning_rate_init",
+
+    train_scores, test_scores = validation_curve(MLPClassifier(), X, y, param,
                                                param_range,
                                                cv=5)
     train_scores_mean = np.mean(train_scores, axis=1)
@@ -41,25 +40,23 @@ def plot_validation_curve(X,y):
     test_scores_std = np.std(test_scores, axis=1)
     title = "MPL"
     plt.title("Validation Curve with MPL")
-    plt.xlabel(r"$learning_rate$")
+    plt.xlabel(r"${}$".format(param))
     plt.ylabel("Score")
     plt.ylim(0.0, 1.1)
     lw = 2
-    plt.semilogx(param_range, train_scores_mean, label="Training score",
+    plt.plot(param_range, train_scores_mean, label="Training score",
                 color="darkorange", lw=lw)
-    plt.fill_between(param_range, train_scores_mean - train_scores_std,
-                    train_scores_mean + train_scores_std, alpha=0.2,
-                    color="darkorange", lw=lw)
-    plt.semilogx(param_range, test_scores_mean, label="Cross-validation score",
+    # plt.fill_between(param_range, train_scores_mean - train_scores_std,
+    #                 train_scores_mean + train_scores_std, alpha=0.2,
+    #                 color="darkorange", lw=lw)
+    plt.plot(param_range, test_scores_mean, label="Cross-validation score",
                 color="navy", lw=lw)
-    plt.fill_between(param_range, test_scores_mean - test_scores_std,
-                    test_scores_mean + test_scores_std, alpha=0.2,
-                    color="navy", lw=lw)
+    # plt.fill_between(param_range, test_scores_mean - test_scores_std,
+    #                 test_scores_mean + test_scores_std, alpha=0.2,
+    #                 color="navy", lw=lw)
     plt.legend(loc="best")
     plt.show()
 
-    plot_learning_curve(MLPClassifier(), title, X, y)
-    plt.show()
 
 def example_plot_learning_curve(X, y):
     # Reference: https://chrisalbon.com/machine_learning/model_evaluation/plot_the_learning_curve/
@@ -115,10 +112,23 @@ def teste_mlp(niter):
         clf.fit(x_train, y_train)
         y_predict = clf.predict(x_test)
         corretudes.append(clf.score(x_test, y_test) * 100)
-    filename = 'teste_MLP_niter={}_epocas={}.png'.format(niter, max_it)
-    title = r'Histograma da execução do MLP, $N={}, $epocas={}$'.format(niter, max_it)
-    save_hist(filename, title, corretudes)
+    # filename = 'teste_MLP_niter={}_epocas={}.png'.format(niter, max_it)
+    # title = r'Histograma da execução do MLP, $N={}, $epocas={}$'.format(niter, max_it)
+    # save_hist(filename, title, corretudes)
     print(corretudes)
 
-niter = 20
-teste_mlp(niter)
+def variar_parametros_mlp(param, param_range):
+    names = ["Freq1", "Freq2", "Freq3", "Freq4", "Freq5", "Freq6", "Freq7", "Freq8", "Freq9", "Classe"]
+    df = pd.read_csv('TmpDataSets/acordes_ordenados.csv', names=names)
+    df_train = pd.read_csv('TmpDataSets/acordes_treino.csv', names=names)
+    df_test = pd.read_csv('TmpDataSets/acordes_teste.csv', names=names)
+    X, y = df[df.columns[:9]], df[df.columns[-1]]
+    df_train = pd.read_csv('TmpDataSets/acordes_treino.csv', names=names)
+    df_test = pd.read_csv('TmpDataSets/acordes_teste.csv', names=names)
+    plot_validation_curve(X, y, param, param_range)
+
+
+# niter = 1
+# teste_mlp(niter)
+
+variar_parametros_mlp("max_iter", [200, 500, 1000, 2000, 8000, 16000, 32000, 64000])
